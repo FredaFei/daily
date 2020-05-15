e9d0e01793131b5a4992451654b4e1167b650f63
fredafei <964959033@qq.com>
2020-05-15 23:12:04 +0800

    styled components run success

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Center} from './1'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Center>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </Center>
         <a
           className="App-link"
           href="https://reactjs.org"
