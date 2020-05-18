2276f1fe9b39c81902910b790362ee1998a046b7
fredafei <964959033@qq.com>
2020-05-18 10:10:50 +0800

    update home

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import Layout from '../components/layout';
 import { Nav } from '../components/nav';
 import { List } from './records.list'
-import { MainButton } from '../components/button/mainButton'
-import { SizedButton } from '../components/button/sizedButton'
-import { MinorButton } from '../components/button/minorButton'
 
 export const Home: React.FC = () => {
   return <Layout hasBack={false} title="鲨鱼记账" footer={<Nav/>}>
     <List/>
-    <MainButton>test</MainButton>
-    <SizedButton>click</SizedButton>
-    <MinorButton>click</MinorButton>
   </Layout>
 };
