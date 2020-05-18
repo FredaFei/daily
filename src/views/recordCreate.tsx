e5a0c446f4460f20089b1dc3f7a79213a75369a2
fredafei <964959033@qq.com>
2020-05-18 19:53:54 +0800

    delete app other

---
+++ src/views/recordCreate.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export const RecordCreate: React.FC = () => {
+  return <Layout hasBack={false} title="record edit">
+    record RecordCreate
+  </Layout>
+};
