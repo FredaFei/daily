8ecf96f4090ea02b0035266d6674f4c11fb57327
fredafei <964959033@qq.com>
2020-05-19 13:43:37 +0800

    add record.tabs

---
+++ src/views/record.tabs.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom'
+import vars from '_vars.scss';
+
+const Wrapper = styled.ul`
+  background: #fff;
+  font-size: 16px;
+  display: flex;
+  >li{
+    flex-grow: 1;
+    >a{
+      height: ${vars.heightTab};
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      &.active::after{
+        content: '';
+        display:block;
+        height: 3px;
+        position:absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        background: ${vars.colorMain}; 
+      }
+    }
+  }
+`
+
+
+export const Tabs = () => {
+  return <Wrapper>
+    <li><NavLink exact to={`/recordExpense/create`} activeClassName="active">支出</NavLink></li>
+    <li><NavLink exact to={`/recordEarn/create`} activeClassName="active">收入</NavLink></li>
+  </Wrapper>
+}
