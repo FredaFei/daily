143b1ca1fce36fc542b49627bfe83857bde08bbf
fredafei <964959033@qq.com>
2020-05-19 13:43:53 +0800

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { Router, Switch, Route, Redirect } from 'react-router-dom'
 import styled from 'styled-components';
 import { SWRConfig } from 'swr'
+import { history } from './lib/history'
+
 import { SignIn } from './views/signIn'
 import { Home } from './views/home'
 import { RecordDetail } from './views/recordDetail'
-import { RecordCreate } from './views/recordCreate'
+import { RecordExpense } from './views/recordExpense'
+import { RecordEarn } from './views/recordEarn'
 import { RecordEdit } from './views/recordEdit'
 import { CostTypeCreate } from './views/costTypeCreate'
 import { CostTypes } from './views/costTypes'
@@ -20,15 +23,16 @@ function App() {
   return (
     <Wrapper>
       <SWRConfig value={{ shouldRetryOnError: false }}>
-        <Router>
+        <Router history={history}>
           <Switch>
             <Route exact path="/signIn"><SignIn/></Route>
             <Route exact path="/home"><Home/></Route>
-            <Route exact path="/record/create"><RecordCreate/></Route>
+            <Route exact path="/recordExpense/create"><RecordExpense/></Route>
+            <Route exact path="/recordEarn/create"><RecordEarn/></Route>
             <Route exact path="/record/:id"><RecordDetail/></Route>
             <Route exact path="/record/:id/edit"><RecordEdit/></Route>
             <Route exact path="/costTypes"><CostTypes/></Route>
-            <Route exact path="/costType/create"><CostTypeCreate/></Route>
+            <Route exact path="/costType/create/:type"><CostTypeCreate/></Route>
             <Route exact path="/me"><Me/></Route>
             <Redirect exact from="/" to="/home"/>
             <Route path="*"></Route>
