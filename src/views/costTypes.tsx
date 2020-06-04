465b8fc9747a8ff0e7cb1e27411360853d3337d2
fredafei <964959033@qq.com>
2020-06-04 22:38:45 +0800

    update costTypes

--- src/views/costTypes.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Layout from '../components/layout';
-
-export const CostTypes: React.FC = () => {
-  return <Layout hasBack={false} title="类别设置">
-    costTypes
-  </Layout>
-};
