274796fcab2adda83a898e4879b4417662e07180
fredafei <964959033@qq.com>
2020-06-04 22:39:03 +0800

    update costTypeCreate

--- src/views/costTypeCreate.tsx
+++ src/views/costTypeCreate.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { HashQuery } from '../lib/getQuery';
 
-export const CostTypeCreate: React.FC = () => {
-  return <Layout hasBack={false} title="添加类别">
+export const _CostTypeCreate: React.FC<RouteComponentProps> = (props) => {
+  const query = new HashQuery<{ type: 'earn' | 'expense' }>(props.location)
+  const type = query.get('type') || 'expense'
+  const map = {
+    earn: '收入',
+    expense: '支出',
+  };
+  const title = `添加${map[type as keyof typeof map]}类别`;
+  return <Layout title={title}>
     CostTypeCreate
   </Layout>
 };
+export const CostTypeCreate = withRouter(_CostTypeCreate);
