0be0a64830876cf5917f5f99e44d87934ab8b5c1
fredafei <964959033@qq.com>
2020-06-06 09:59:28 +0800

    update costTypeCreate

--- src/views/costTypeCreate.tsx
+++ src/views/costTypeCreate.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from '../components/layout';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { withRouter, RouteComponentProps, useLocation } from 'react-router-dom';
 import { HashQuery } from '../lib/getQuery';
 import styled from 'styled-components';
 
 import Icon from '../components/icon';
 import { IconName } from '../components/iconName'
 import { ShapedDiv } from '../components/shapedDiv'
+import { useCostType } from '../hooks/useCostType';
 
 const TopInput = styled.div`
   display: flex;
@@ -27,8 +28,30 @@ const TopInput = styled.div`
     }
   }
 `
-const CostTypeItem = styled.div`
 
+const CostType = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`
+const Section = styled.section`
+  margin: 12px 0;
+`
+
+const Title = styled.div`
+  font-size: 18px;
+  text-align: center;
+  margin-bottom: 12px;
+`
+const CostTypeItem = styled.div`
+  width: 24%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 8px;
+  &:not(:nth-child(4n)) {
+    margin-right: calc(4% / 3);
+  }
 `
 export const _CostTypeCreate: React.FC<RouteComponentProps> = (props) => {
   const query = new HashQuery<{ type: 'earn' | 'expense' }>(props.location)
@@ -38,6 +61,12 @@ export const _CostTypeCreate: React.FC<RouteComponentProps> = (props) => {
     expense: '支出',
   };
   const title = `添加${map[type as keyof typeof map]}类别`;
+
+  const { costType } = useCostType(0)
+  console.log(useLocation());
+  const onItemClick = (item: CostType, index: number) => {
+    // todo
+  }
   return <Layout title={title} footer={false}>
     <TopInput>
       <ShapedDiv>
@@ -45,7 +74,30 @@ export const _CostTypeCreate: React.FC<RouteComponentProps> = (props) => {
       </ShapedDiv>
       <input type="text" placeholder="输入类别名称(不超过四个汉字)"/>
     </TopInput>
-    <CostTypeItem></CostTypeItem>
+    <Section>
+      <Title>娱乐</Title>
+      <CostType>
+        {costType && costType.length > 0 && costType.map((cost, index) => (
+          <CostTypeItem key={cost.id} onClick={() => onItemClick(cost, index)}>
+            <ShapedDiv>
+              <Icon name={cost.icon}/>
+            </ShapedDiv>
+          </CostTypeItem>
+        ))}
+      </CostType>
+    </Section>
+    <Section>
+      <Title>饮食</Title>
+      <CostType>
+        {costType && costType.length > 0 && costType.map((cost, index) => (
+          <CostTypeItem key={cost.id} onClick={() => onItemClick(cost, index)}>
+            <ShapedDiv>
+              <Icon name={cost.icon}/>
+            </ShapedDiv>
+          </CostTypeItem>
+        ))}
+      </CostType>
+    </Section>
   </Layout>;
 };
 export const CostTypeCreate = withRouter(_CostTypeCreate);
