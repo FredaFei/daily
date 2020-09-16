fcfbd25c7e02593155f099e7b52d2aea46ae3af9
fh <feih@angelcomm.cn>
2020-09-16 17:20:37 +0800

    接入API请求

--- src/lib/http.tsx
+++ src/lib/http.tsx
@@ -1,74 +1,74 @@
-import axios, { AxiosInstance, AxiosError, AxiosRequestConfig } from 'axios'
+import axios, { AxiosInstance, AxiosError, AxiosRequestConfig } from 'axios';
 
 export interface RequestConfig extends AxiosRequestConfig {
-  autoHandlerError?: boolean | ((error: any) => boolean)
+    autoHandlerError?: boolean | ((error: any) => boolean)
 }
 
 class Http {
-  client: AxiosInstance;
+    client: AxiosInstance;
 
-  constructor(public baseUrl: string, public handleError?: (error: any) => Promise<never>) {
-    this.client = axios.create({
-      baseURL: baseUrl,
-      timeout: 10 * 1000,
-      headers: {},
-      withCredentials: true
-    })
-  }
+    constructor(public baseUrl: string, public handleError?: (error: any) => Promise<never>) {
+        this.client = axios.create({
+            baseURL: baseUrl,
+            timeout: 10 * 1000,
+            headers: {},
+            withCredentials: true
+        });
+    }
 
-  ajax<T>(options: RequestConfig) {
-    const { method, url, data, autoHandlerError, ...config } = options
-    const promise =
-      method === 'get' ? this.client.get<T>(url!, config) :
-        method === 'post' ? this.client.post<T>(url!, data, config) :
-          method === 'patch' ? this.client.patch<T>(url!, data, config) :
-            method === 'delete' ? this.client.delete<T>(url!, config) : undefined as never
-    return promise.then(null, error => {
-      if ((typeof autoHandlerError === 'function' ? autoHandlerError(error) : autoHandlerError) && this.handleError) {
-        return this.handleError(error)
-      } else {
-        return Promise.reject(error)
-      }
-    })
-  }
+    ajax<T>(options: RequestConfig) {
+        const { method, url, data, autoHandlerError, ...config } = options;
+        const promise =
+            method === 'get' ? this.client.get<T>(url!, config) :
+                method === 'post' ? this.client.post<T>(url!, data, config) :
+                    method === 'patch' ? this.client.patch<T>(url!, data, config) :
+                        method === 'delete' ? this.client.delete<T>(url!, config) : undefined as never;
+        return promise.then(null, error => {
+            if ((typeof autoHandlerError === 'function' ? autoHandlerError(error) : autoHandlerError) && this.handleError) {
+                return this.handleError(error);
+            } else {
+                return Promise.reject(error);
+            }
+        });
+    }
 
-  get<T>(url: string, options?: RequestConfig) {
-    return this.ajax<T>({ ...options, url, method: 'get' })
-  }
+    get<T>(url: string, options?: RequestConfig) {
+        return this.ajax<T>({ ...options, url, method: 'get' });
+    }
 
-  post<T>(url: string, data?: unknown, options?: RequestConfig) {
-    return this.ajax<T>({ ...options, url, data, method: 'post' })
-  }
+    post<T>(url: string, data?: unknown, options?: RequestConfig) {
+        console.log('post', data);
+        return this.ajax<T>({ ...options, url, data, method: 'post' });
+    }
 
-  patch<T>(url: string, data?: unknown, options?: RequestConfig) {
-    return this.ajax<T>({ ...options, url, data, method: 'patch' })
-  }
+    patch<T>(url: string, data?: unknown, options?: RequestConfig) {
+        return this.ajax<T>({ ...options, url, data, method: 'patch' });
+    }
 
-  delete<T>(url: string, options?: RequestConfig) {
-    return this.ajax<T>({ ...options, url, method: 'delete' })
-  }
+    delete<T>(url: string, options?: RequestConfig) {
+        return this.ajax<T>({ ...options, url, method: 'delete' });
+    }
 
 }
 
-const isDev = () => window.location.hostname === 'localhost'
-const
-  defaultHttp = new Http(
-    isDev() ? 'http://rap2.taobao.org:38080/app/mock/254030' : '',
+const isDev = () => window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const defaultHttp = new Http(
+    '',
     error => {
-      if (error.isAxiosError) {
-        const { response } = error as AxiosError
-        if (response === undefined) {
-          console.log('network error');
-        } else if (response?.status === 401) {
-          console.log('sign in');
-        } else if (response?.status === 404) {
-          console.log('not found');
-        } else if (response?.status >= 400) {
-          console.log('server busy');
+        if (error.isAxiosError) {
+            const { response } = error as AxiosError;
+            if (response === undefined) {
+                console.log('network error');
+            } else if (response?.status === 401) {
+                console.log('sign in');
+            } else if (response?.status === 404) {
+                console.log('not found');
+            } else if (response?.status >= 400) {
+                console.log('server busy');
+            }
         }
-      }
-      return Promise.reject(error)
+        return Promise.reject(error);
     }
-  )
+);
 
-export { defaultHttp, Http }
+export { defaultHttp, Http };
--- src/views/signIn.tsx
+++ src/views/signIn.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {} from 'react';
 import Layout from '../components/layout';
+import { useSignUp } from '../hooks/useSignUp';
 
 export const SignIn: React.FC = () => {
-  return <Layout hasBack={false} title="SignIn">
-    SignIn
-  </Layout>
+    const onSignInOrUp = () => {
+    };
+    const { user } = useSignUp({ email: '3@qq.com', password: '123456', password_confirmation: '123456' });
+    console.log(user);
+    return <Layout hasBack={false} title="SignIn">
+        SignIn
+        <button onClick={onSignInOrUp}>SignIn</button>
+    </Layout>;
 };
